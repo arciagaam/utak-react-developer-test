1f749be6fb689596404aec2ab0ed4a19ce72e344
Miguel <miguelarciagaa@gmail.com>
2024-02-23 00:20:08 +0800

    firebase.ts fix

--- src/database/firebase.ts
+++ src/database/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: "AIzaSyD_n5_xrvSlwcrXei_FQMSlx4iYLyJzVqA",
+  authDomain: "utak-menu.firebaseapp.com",
+  databaseURL: "https://utak-menu-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "utak-menu",
+  storageBucket: "utak-menu.appspot.com",
+  messagingSenderId: "411533428267",
+  appId: "1:411533428267:web:f64a9e7a17c69f8f914c6e",
 };
 
 const app = initializeApp(firebaseConfig);
